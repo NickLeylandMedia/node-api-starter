e4566dba09128ede4334cc2bbe43bdd56256bda5
NickLeylandMedia <nick@nick-leyland.com>
2022-11-08 01:54:55 -0500

    Made controller functions asynchronous and made them return explicit values.

--- src/controllers/sample_cont.ts
+++ src/controllers/sample_cont.ts
@@ -1,27 +1,35 @@
 import { Request, Response, NextFunction } from "express";
 
-const addMeal = (req: Request, res: Response, next: NextFunction) => {
-  res.json({ message: "New meal posted" });
+const addMeal = async (req: Request, res: Response, next: NextFunction) => {
+  return res.json({ message: "New meal posted" });
 };
 
-const deleteAllMeals = (req: Request, res: Response, next: NextFunction) => {
-  res.json({ message: "All meals deleted" });
+const deleteAllMeals = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  return res.json({ message: "All meals deleted" });
 };
 
-const deleteOneMeal = (req: Request, res: Response, next: NextFunction) => {
-  res.json({ message: "Meal deleted" });
+const deleteOneMeal = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  return res.json({ message: "Meal deleted" });
 };
 
-const getAllMeals = (req: Request, res: Response, next: NextFunction) => {
-  res.json({ message: "Here are the meals!" });
+const getAllMeals = async (req: Request, res: Response, next: NextFunction) => {
+  return res.json({ message: "Here are the meals!" });
 };
 
-const getOneMeal = (req: Request, res: Response, next: NextFunction) => {
-  res.json({ message: "Here is the meal!" });
+const getOneMeal = async (req: Request, res: Response, next: NextFunction) => {
+  return res.json({ message: "Here is the meal!" });
 };
 
-const modifyMeal = (req: Request, res: Response, next: NextFunction) => {
-  res.json({ message: "Meal modified" });
+const modifyMeal = async (req: Request, res: Response, next: NextFunction) => {
+  return res.json({ message: "Meal modified" });
 };
 
 export default {
